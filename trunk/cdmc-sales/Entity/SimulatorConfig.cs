a965de84b7371426ae8150000ab2ab7c1e401d1c
yanqinqiang521@gmail.com <yanqinqiang521@gmail.com@9611b860-a7ab-5213-ba63-1c75deae6885>
2013-05-11 04:19:29 +0000

    Missed SimulatorConfig file

---
+++ trunk/cdmc-sales/Entity/SimulatorConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity
+{
+    /// <summary>
+    /// Entity of simulator config.
+    /// </summary>
+    public class SimulatorConfig : EntityBase
+    {
+        public string AdminName { get; set; }
+
+        [Display(Name = "模拟人"), Required]
+        public string SimulatorName { get; set; }
+    }
+}
